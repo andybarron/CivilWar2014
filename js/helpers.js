86fa17549a4c8a4b1c855adac1f06a22e0ac6c6c
davidamin <amin.david.m@gmail.com>
2014-04-23 21:31:49 -0400

    Basis of "graph game"
    The very first steps toward a working graph minigame. Still need to add
    the real art assets, add a full set of nodes, construct logic to detect
    victory, add a rudimentary AI, and write code to handle the "special"
    nodes.

--- js/helpers.js
+++ js/helpers.js
@@ -90,6 +90,35 @@ function recTouch(a,b,error) // error > 0 is easier to hit (bigger)
 	}
 }
 
+function wasClicked(a,b,error) // error > 0 is easier to hit (bigger)
+{
+	if(!exists(error))
+	{
+		error = 0;
+	}
+
+	var L1 = a.x - error;
+	var L2 = b.x;
+
+	var R1 = a.x + a.width + error;
+	//var R2 = b.x + b.width;
+
+	var B1 = a.y - error;
+	var B2 = b.y;
+
+	var T1 = a.y + a.height + error;
+	//var T2 = b.y + b.height;
+
+	if(L2>L1 && L2<R1 && B2>B1 && B2<T1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 function spriteZSort(a,b)
 {
 	if(a.position.y < b.position.y)
